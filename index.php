caefa04c5cadf65d8310d3d2977881770b876ebe
marigopal <88581007+marigopal@users.noreply.github.com>
2022-02-08 13:18:18 +0530

    Template_files

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <script type="text/javascript">
+            window.location.href = "home/"
+        </script>
+    </body>
+</html>
